903bf555bc575cd66274fca3ee781aeafbd80b9e
emesc <tahder77@gmail.com>
2017-01-04 16:16:15 +1100

    Added ancestry to comment model and table.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :restaurant
+  validates :restaurant, presence: true
+  has_ancestry
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104045504) do
+ActiveRecord::Schema.define(version: 20170104050539) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170104045504) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "ancestry"
+    t.index ["ancestry"], name: "index_comments_on_ancestry"
     t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
   end
 
